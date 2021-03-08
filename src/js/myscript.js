957c3596aba6e191ed7b791f4bd37dc9a40b840a
Munavar Harris <60294955+mharris-t@users.noreply.github.com>
2021-03-08 02:19:22 +0200

    Delete myscript.js

--- src/js/myscript.js
+++
@@ -1,9 +0,0 @@
-const visitorcount_api_url = 'https://g7tsy9xtg6.execute-api.us-east-1.amazonaws.com/Prod/myVisitCounter/'
-async function getData() {
-    const response = await fetch(visitorcount_api_url);
-    const data = await response.json();
-    const {WebsiteCounter} = data;
-    document.getElementById('mycounter').textContent = WebsiteCounter;
-    console.log(data);
-}
-getData();
