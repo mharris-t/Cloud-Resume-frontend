966d37d1f79dfc730f6315a831928ca4bd5c4f16
Munavar Harris <60294955+mharris-t@users.noreply.github.com>
2021-03-08 02:20:14 +0200

    updated script

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -35,3 +35,13 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+const visitorcount_api_url = 'https://g7tsy9xtg6.execute-api.us-east-1.amazonaws.com/Prod/myVisitCounter/'
+async function getData() {
+    const response = await fetch(visitorcount_api_url);
+    const data = await response.json();
+    const {WebsiteCounter} = data;
+    document.getElementById('mycounter').textContent = WebsiteCounter;
+    console.log(data);
+}
+getData();
